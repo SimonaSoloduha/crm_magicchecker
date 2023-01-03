11177965d8ae2eef04068aab615ad15fb95c9535
Simona Soloduha <ls460simona@gmail.com>
2023-01-03 17:58:35 +0200

    Fix

--- web/index.php
+++ web/index.php
@@ -231,3 +231,8 @@ else {
         }
     }
 }
+
+$app->get('/cowsay', function() use($app) {
+    $app['monolog']->addDebug('cowsay');
+    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
