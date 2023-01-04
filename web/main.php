23b820b299cb98c1dd4774962ae8d32c28a8e815
Simona Soloduha <ls460simona@gmail.com>
2023-01-04 11:43:50 +0200

    «test1»

---
+++ web/main.php
@@ -0,0 +1,8 @@
+<?php
+
+
+function text()
+{
+    echo '<html><body><h1>MAIN</h1></body></html>';
+}
+
---
+++ web/yes.php
@@ -0,0 +1,7 @@
+<?php
+
+
+function text()
+{
+    echo '<html><body><h1>YES </h1></body></html>';
+}
