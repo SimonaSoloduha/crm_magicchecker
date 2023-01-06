9004ef6e1bc1c4db537a852fd47691b2b9e6e057
Simona Soloduha <ls460simona@gmail.com>
2023-01-06 11:45:49 +0200

    «test1»

--- web/main.php
+++ web/main.php
@@ -1,4 +1,4 @@
 <?php
 
-header('Location: https://damp-everglades-01529.herokuapp.com/logs/?$MAIN');
+header('Location: https://damp-everglades-01529.herokuapp.com/logs/?$MAIN'.'?'.$_SERVER['QUERY_STRING']);
 exit();
--- web/yes.php
+++ web/yes.php
@@ -1,4 +1,4 @@
 <?php
 
-header('Location: https://damp-everglades-01529.herokuapp.com/logs/?$YES');
+header('Location: https://damp-everglades-01529.herokuapp.com/logs/?$YES'.'?'.$_SERVER['QUERY_STRING']);
 exit();
