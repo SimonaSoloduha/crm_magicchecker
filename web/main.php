6820d5e1e5f78c1631fff760f3ed0937e3c679c6
Simona Soloduha <ls460simona@gmail.com>
2023-01-09 10:40:09 +0200

    «test1»

--- web/main.php
+++ web/main.php
@@ -3,5 +3,5 @@ $url = $_SERVER['QUERY_STRING'];
 $url_components = parse_url($url);
 
 parse_str($url_components['query'], $params);
-header('Location: http://127.0.0.1:8000/checker/?&filter2=MAIN?&id=' . $params['id']);
+header('Location: http://127.0.0.1:8000/checker/?&magicchecker=MAIN&id=' . $params['id']);
 exit();
