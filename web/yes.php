c93ca1b4412e4c0c159246e21c432b585aab22e5
Simona Soloduha <ls460simona@gmail.com>
2023-01-12 15:14:24 +0200

    «test1»

--- web/yes.php
+++ web/yes.php
@@ -3,5 +3,8 @@ $url = $_SERVER['QUERY_STRING'];
 $url_components = parse_url($url);
 
 parse_str($url_components['query'], $params);
-header('Location: https://damp-everglades-01529.herokuapp.com/checker/?&magicchecker=YES&id=' . $params['id']);
+
+header('Location: http://127.0.0.1:8000/checker/?&magicchecker=YES&id=' . $params['id']);
+
+//header('Location: https://damp-everglades-01529.herokuapp.com/checker/?&magicchecker=YES&id=' . $params['id']);
 exit();
