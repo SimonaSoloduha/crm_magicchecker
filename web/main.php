5deb34da783e7e6139a047a55a1df05b4e08aae5
Simona Soloduha <ls460simona@gmail.com>
2023-01-12 17:42:02 +0200

    «test1»

--- web/main.php
+++ web/main.php
@@ -3,7 +3,7 @@ $url = $_SERVER['QUERY_STRING'];
 $url_components = parse_url($url);
 
 parse_str($url_components['query'], $params);
-header('Location: http://127.0.0.1:8000/checker/?&magicchecker=MAIN&id=' . $params['id']);
+//header('Location: http://127.0.0.1:8000/checker/?&magicchecker=MAIN&id=' . $params['id']);
 
-//header('Location: https://damp-everglades-01529.herokuapp.com/checker/?&magicchecker=MAIN&id=' . $params['id']);
+header('Location: https://damp-everglades-01529.herokuapp.com/checker/?&magicchecker=MAIN&id=' . $params['id']);
 exit();
--- web/yes.php
+++ web/yes.php
@@ -4,7 +4,7 @@ $url_components = parse_url($url);
 
 parse_str($url_components['query'], $params);
 
-header('Location: http://127.0.0.1:8000/checker/?&magicchecker=YES&id=' . $params['id']);
+//header('Location: http://127.0.0.1:8000/checker/?&magicchecker=YES&id=' . $params['id']);
 
-//header('Location: https://damp-everglades-01529.herokuapp.com/checker/?&magicchecker=YES&id=' . $params['id']);
+header('Location: https://damp-everglades-01529.herokuapp.com/checker/?&magicchecker=YES&id=' . $params['id']);
 exit();
